52f9b9a180ecc5927f3d2a33d5fdd73dae249df0
Kim Kahns <kahns@bdrops.de>
2018-08-08 11:22:35 +0200

    added doctrine type

---
+++ Form/Types/DoctrineType.php
@@ -0,0 +1,69 @@
+<?php
+
+declare(strict_types=1);
+
+namespace RevisionTen\CMS\Form\Types;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class DoctrineType extends AbstractType
+{
+    /** @var EntityManagerInterface */
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        parent::buildForm($builder, $options);
+
+        $choices = [];
+
+        if ($options['findBy']) {
+            /** @var array $entities */
+            $entities = $this->entityManager->getRepository($options['entityClass'])->findBy($options['findBy']);
+        } else {
+            /** @var array $entities */
+            $entities = $this->entityManager->getRepository($options['entityClass'])->findAll();
+        }
+
+        if ($entities) {
+            foreach ($entities as $entity) {
+                $choices[(string) $entity] = $entity->getId();
+            }
+        }
+
+        $builder->add('entityId', ChoiceType::class, [
+            'required' => $options['required'],
+            'label' => false,
+            'choices' => $choices,
+        ]);
+        $builder->add('entityClass', HiddenType::class, [
+            'data' => $options['entityClass'],
+            'empty_data' => $options['entityClass'],
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'required' => true,
+            'entityClass' => null,
+            'findBy' => false,
+        ]);
+    }
+}
