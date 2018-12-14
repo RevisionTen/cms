b3e31d9a92240690b3b6bb628f6db5ffcd1e50b4
Kim Kahns <kahns@bdrops.de>
2018-12-14 11:02:23 +0100

    fixed bug in install roles command

--- CmsBundle.php
+++ CmsBundle.php
@@ -7,7 +7,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 class CmsBundle extends Bundle
 {
-    public const VERSION = '1.4.0';
+    public const VERSION = '1.4.1';
 
     /**
      * {@inheritdoc}
