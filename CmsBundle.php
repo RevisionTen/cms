617456798436c1845f5b658c9c39f6d422bc3929
Kim Kahns <kahns@bdrops.de>
2018-12-17 17:10:04 +0100

    fixed form styling problems

--- CmsBundle.php
+++ CmsBundle.php
@@ -7,7 +7,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 class CmsBundle extends Bundle
 {
-    public const VERSION = '1.4.6';
+    public const VERSION = '1.4.7';
 
     /**
      * {@inheritdoc}
