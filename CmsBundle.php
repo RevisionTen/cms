ff44c1031af7b63700a65c88b1497c9f579130c0
Cornelius K <kahns@bdrops.de>
2019-02-08 17:02:54 +0100

    removed modal fade

--- CmsBundle.php
+++ CmsBundle.php
@@ -9,7 +9,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 class CmsBundle extends Bundle
 {
-    public const VERSION = '1.4.25';
+    public const VERSION = '1.4.26';
 
     /**
      * {@inheritdoc}
