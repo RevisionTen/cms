5bc2e5ff846d62626b33c2baa94a0bdfce960219
Cornelius K <kahns@bdrops.de>
2019-04-05 13:51:46 +0200

    added bootstrap button styles to ckeditor

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,10 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [1.5.14] - 2019-04-05
+### Added
+- Added bootstrap button styles to ckeditor
+
 ## [1.5.13] - 2019-04-05
 ### Added
 - Added 404 error page option to website settings **Update your database schema**
--- Resources/public/js/ckeditor-styles.js
+++ Resources/public/js/ckeditor-styles.js
@@ -5,7 +5,15 @@ CKEDITOR.stylesSet.add('bootstrap4styles', [
     { name: 'align right', element: blockElements, attributes: { 'class': 'text-right' } },
     { name: 'align center', element: blockElements, attributes: { 'class': 'text-center' } },
     { name: 'justify', element: blockElements, attributes: { 'class': 'text-justify' } },
-    { name: 'small', element: 'small' }
+    { name: 'small', element: 'small' },
+    { name: 'button primary', element: 'a', attributes: { 'class': 'btn btn-primary' } },
+    { name: 'button secondary', element: 'a', attributes: { 'class': 'btn btn-secondary' } },
+    { name: 'button dark', element: 'a', attributes: { 'class': 'btn btn-dark' } },
+    { name: 'button light', element: 'a', attributes: { 'class': 'btn btn-light' } },
+    { name: 'button danger', element: 'a', attributes: { 'class': 'btn btn-danger' } },
+    { name: 'button warning', element: 'a', attributes: { 'class': 'btn btn-warning' } },
+    { name: 'button info', element: 'a', attributes: { 'class': 'btn btn-info' } },
+    { name: 'button success', element: 'a', attributes: { 'class': 'btn btn-success' } }
 ]);
 CKEDITOR.dtd.$removeEmpty.span = 0;
 CKEDITOR.dtd.$removeEmpty.i = 0;
