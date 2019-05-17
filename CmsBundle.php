4c98bf04bce0e3ec9b45c4374cdfb6ab5002ecb0
Cornelius K <kahns@bdrops.de>
2019-05-17 13:19:59 +0200

    Fixed website entity editing bug

--- CmsBundle.php
+++ CmsBundle.php
@@ -9,7 +9,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 class CmsBundle extends Bundle
 {
-    public const VERSION = '1.5.29';
+    public const VERSION = '1.5.30';
 
     /**
      * {@inheritdoc}
