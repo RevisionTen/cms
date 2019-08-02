1372f8c27096e29d42681dc3b1eb88eeeeee3695
Cornelius K <kahns@bdrops.de>
2019-08-02 13:40:19 +0200

    Added page history to page inspect modal

--- CmsBundle.php
+++ CmsBundle.php
@@ -9,7 +9,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 class CmsBundle extends Bundle
 {
-    public const VERSION = '1.7.7';
+    public const VERSION = '1.7.8';
 
     /**
      * {@inheritdoc}
