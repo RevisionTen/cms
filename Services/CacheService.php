6d164e17947ae406ac7fd62123bf57067f3889c8
Cornelius K <kahns@bdrops.de>
2019-09-19 16:45:03 +0200

    Added disable_cache_workaround option

--- Services/CacheService.php
+++ Services/CacheService.php
@@ -49,15 +49,26 @@ class CacheService
     private $cache;
 
     /**
+     * @var bool
+     */
+    private $disableCacheWorkaround;
+
+    /**
      * CacheService constructor.
      *
      * @param array $config
      */
     public function __construct(array $config)
     {
-        $this->issuer = $config['site_name'] ?? 'revisionTen';
+        $apcEnabled = extension_loaded('apcu') && ini_get('apc.enabled');
 
-        if (function_exists('shm_attach') && extension_loaded('apcu') && ini_get('apc.enabled')) {
+        $this->issuer = $config['site_name'] ?? 'revisionTen';
+        $this->disableCacheWorkaround = (bool) ($config['disable_cache_workaround'] ?? false);
+
+        if ($apcEnabled && $this->disableCacheWorkaround) {
+            $this->cache = new ApcuAdapter();
+            $this->initUuidStore();
+        } elseif ($apcEnabled && function_exists('shm_attach')) {
             $this->cache = new ApcuAdapter();
 
             try {
@@ -74,32 +85,46 @@ class CacheService
         }
     }
 
-    private function uuidStoreExists(): bool
+    private function uuidStoreSharedMemorySegmentExists(): bool
     {
         return !is_bool($this->shmSegment) && shm_has_var($this->shmSegment, $this->shmVarKey);
     }
 
     private function initUuidStore(): void
     {
-        if (!$this->uuidStoreExists()) {
-            // UuidStore does not exist, create UuidStore.
-            shm_put_var($this->shmSegment, $this->shmVarKey, $this->uuidStore);
+        if ($this->disableCacheWorkaround) {
+            // Get from APCu.
+            $uuidStoreCacheEntry = $this->cache->getItem($this->issuer.'_uuidStore');
+            if ($uuidStoreCacheEntry->isHit()) {
+                $this->uuidStore = $uuidStoreCacheEntry->get();
+            }
+        } else {
+            if (!$this->uuidStoreSharedMemorySegmentExists()) {
+                // UuidStore does not exist, create UuidStore.
+                shm_put_var($this->shmSegment, $this->shmVarKey, $this->uuidStore);
+            }
+
+            if (!$this->uuidStoreSharedMemorySegmentExists()) {
+                // Failed to create UuidStore, disable cache.
+                $this->cache = null;
+
+                return;
+            }
+
+            // Get from shared memory segment.
+            $this->uuidStore = shm_get_var($this->shmSegment, $this->shmVarKey);
         }
-
-        if (!$this->uuidStoreExists()) {
-            // Failed to create UuidStore, disable cache.
-            $this->cache = null;
-
-            return;
-        }
-
-        // UuidStore exists, get it.
-        $this->uuidStore = shm_get_var($this->shmSegment, $this->shmVarKey);
     }
 
     private function saveUuidStore(): void
     {
-        if ($this->shmSegment) {
+        if ($this->disableCacheWorkaround) {
+            // Save to APCu.
+            $uuidStoreCacheEntry = $this->cache->getItem($this->issuer.'_uuidStore');
+            $uuidStoreCacheEntry->set($this->uuidStore);
+            $this->cache->save($uuidStoreCacheEntry);
+        } elseif ($this->shmSegment)  {
+            // Save to shared memory segment.
             shm_put_var($this->shmSegment, $this->shmVarKey, $this->uuidStore);
         }
     }
@@ -170,7 +195,7 @@ class CacheService
         // Save current version to memory and return the save version.
         $saveVersion = $this->setVersion($uuid, $version);
 
-        // Save data to apc cache.
+        // Save data to APCu cache.
         $entry = $this->cache->getItem($this->issuer.'_'.$uuid.'_v'.$saveVersion);
         $entry->set($data);
 
