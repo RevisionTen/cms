52d478704ba7b09acada2f6052504c44bb36103c
Cornelius K <kahns@bdrops.de>
2019-10-09 12:30:47 +0200

    Changed image picker sorting

--- CmsBundle.php
+++ CmsBundle.php
@@ -9,7 +9,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 class CmsBundle extends Bundle
 {
-    public const VERSION = '2.0.10';
+    public const VERSION = '2.0.11';
 
     /**
      * {@inheritdoc}
