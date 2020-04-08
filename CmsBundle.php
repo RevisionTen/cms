46bc05060f568f1f664d8221780e6245b3101a81
Cornelius K <kahns@bdrops.de>
2020-04-08 10:31:50 +0200

    Security updates and bugfixes

--- CmsBundle.php
+++ CmsBundle.php
@@ -9,7 +9,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 class CmsBundle extends Bundle
 {
-    public const VERSION = '2.2.7';
+    public const VERSION = '2.2.8';
 
     /**
      * {@inheritdoc}
