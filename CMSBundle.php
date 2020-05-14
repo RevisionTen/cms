3dd0440482d6030a4248e92995956d827a44bc56
Cornelius K <kahns@bdrops.de>
2020-05-14 13:12:06 +0200

    Bugfixes

--- CMSBundle.php
+++ CMSBundle.php
@@ -9,7 +9,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 class CMSBundle extends Bundle
 {
-    public const VERSION = '2.3.2';
+    public const VERSION = '2.3.3';
 
     /**
      * {@inheritdoc}
