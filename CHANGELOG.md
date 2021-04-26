a04cc77dbd1f15e83d8c66381ba8dcaedd2c0052
TAR5 <jessiepopel@icloud.com>
2021-04-26 09:46:46 +0200

    Added solr_username and solr_password options to CMS config

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,10 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [2.3.24] - 2021-04-26
+### Changed
+- Added `solr_username` and `solr_password` options to CMS config.
+
 ## [2.3.23] - 2021-02-24
 ### Changed
 - Added missing `symfony/mime` dependency
--- Services/IndexService.php
+++ Services/IndexService.php
@@ -68,15 +68,22 @@ class IndexService
         $this->pageService = $pageService;
         $this->config = $config;
 
+        $endpoint = [
+            'host' => $config['solr_host'],
+            'port' => $config['solr_port'],
+            'path' => '/',
+            'collection' => $config['solr_collection'],
+        ];
+
+        if (!empty($config['solr_username']) && !empty($config['solr_password'])) {
+            $endpoint['username'] = $config['solr_username'];
+            $endpoint['password'] = $config['solr_password'];
+        }
+
         $this->solrConfig = $config['solr_collection'] ? [
             'endpoint' => [
-                'localhost' => [
-                    'host' => $config['solr_host'],
-                    'port' => $config['solr_port'],
-                    'path' => '/',
-                    'collection' => $config['solr_collection'],
-                ]
-            ]
+                'localhost' => $endpoint,
+            ],
         ] : null;
     }
 
