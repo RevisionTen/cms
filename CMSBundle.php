250360bde1059fc9aeafa4390c7360b9150b7ca0
Cornelius Kahns <kahns@bdrops.de>
2022-01-18 16:38:23 +0100

    Bugfixes

--- CMSBundle.php
+++ CMSBundle.php
@@ -19,7 +19,7 @@ use function class_alias;
 
 class CMSBundle extends Bundle
 {
-    public const VERSION = '3.0.19';
+    public const VERSION = '3.0.20';
 }
 
 class_alias(Domain::class, '\\RevisionTen\\CMS\\Model\\Domain');
