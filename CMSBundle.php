07d099c99fca6805a139da7342c3f46aacf97943
Cornelius Kahns <67231872+TAR5@users.noreply.github.com>
2026-01-12 12:16:05 +0100

    PHP 8.3 compatibility

--- CMSBundle.php
+++ CMSBundle.php
@@ -19,7 +19,7 @@ use function class_alias;
 
 class CMSBundle extends Bundle
 {
-    public const VERSION = '3.8.9';
+    public const VERSION = '3.9.2';
 }
 
 class_alias(Domain::class, '\\RevisionTen\\CMS\\Model\\Domain');
